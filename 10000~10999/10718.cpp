19b5569c612de6067a74d23a22c3d95aed70b343
ntxq <bkwak@ust.hk>
2022-04-02 10:39:38 +0800

    2022-04-02

---
+++ 10000~10999/10718.cpp
@@ -0,0 +1,11 @@
+#include <algorithm>
+#include <iostream>
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+
+    std::cout << "강한친구 대한육군\n강한친구 대한육군";
+
+    return 0;
+}
