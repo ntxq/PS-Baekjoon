71aef35dd7da8dee9f350bb1fee291ea96be2a44
ntxq <bkwak@ust.hk>
2022-04-03 10:19:18 +0800

    2022-04-03

---
+++ 10000~10999/10926.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+
+    std::string s;
+    std::cin >> s;
+    std::cout << s << "\?\?!";
+
+    return 0;
+}
