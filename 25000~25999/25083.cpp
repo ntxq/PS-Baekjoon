72d8eabf306b0d29d5070b8e7eb57bc266411dfb
ntxq <bkwak@ust.hk>
2022-05-07 09:19:11 +0800

    2022-05-07

---
+++ 25000~25999/25083.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+
+    std::cout << "         ,r'\"7\n"
+              << "r`-_   ,'  ,/\n"
+              << " \\. \". L_r'\n"
+              << "   `~\\/\n"
+              << "      |\n"
+              << "      |\n";
+
+    return 0;
+}
