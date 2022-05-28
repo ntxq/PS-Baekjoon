5f1aa30410d8c9dcf59cefc3c66904c09c4c3051
ntxq <bkwak@ust.hk>
2022-05-28 09:20:29 +0800

    2022-05-28

---
+++ 2000~2999/2440.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+
+    int N;
+    std::cin >> N;
+
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N - i; ++j) std::cout << '*';
+        std::cout << '\n';
+    }
+
+    return 0;
+}
