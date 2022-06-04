4462135d6186be969913933e53f5af1a354bbf2b
ntxq <bkwak@ust.hk>
2022-06-04 18:33:28 +0900

    2022-06-04

---
+++ 7000~7999/7287.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <iostream>
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+
+    std::cout << 381 << '\n' << "bkwak" << '\n';
+    return 0;
+}
