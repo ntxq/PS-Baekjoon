3dfa30f76d8e4b00af7e9ffed9a64dec9533078b
ntxq <qudrhs99@gmail.com>
2023-03-21 09:38:15 +0800

    2023-03-21

---
+++ 15000~15999/15894.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <numeric>
+#include <vector>
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+
+    long long N;
+    std::cin >> N;
+
+    std::cout << 4 * N << '\n';
+
+
+    return 0;
+}
