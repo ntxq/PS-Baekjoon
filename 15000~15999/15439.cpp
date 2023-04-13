012b153cefe0ace8b46f528a5b6d67bd8efc44ce
ntxq <qudrhs99@gmail.com>
2023-04-13 09:21:24 +0800

    2023-04-13

---
+++ 15000~15999/15439.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+#include <limits>
+#include <numeric>
+#include <vector>
+
+int main() {
+    std::cin.tie(nullptr);
+    std::ios_base::sync_with_stdio(false);
+
+    int N;
+    std::cin >> N;
+
+    std::cout << N * (N - 1) << '\n';
+
+    return 0;
+}
