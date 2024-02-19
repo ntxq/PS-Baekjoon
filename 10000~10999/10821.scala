921c62303d5c6339aa992b68eaf7d91668db4110
bkwak <qudrhs99@gmail.com>
2024-02-19 09:50:30 +0800

    2024-02-19

---
+++ 10000~10999/10821.scala
@@ -0,0 +1,12 @@
+import typings.node.{fsMod as fs}
+
+import scala.scalajs.js.annotation.*
+
+object Main:
+  def main(args: Array[String]): Unit =
+    val input  = fs.readFileSync("/dev/stdin").toString.trim.linesIterator.toList
+    val output = solve(input)
+    fs.writeFileSync("/dev/stdout", s"$output\n")
+
+  def solve(input: List[String]): String =
+    input.head.split(",").length.toString
